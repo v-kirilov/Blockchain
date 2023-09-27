9a4038112e5ca583c71eae8e10bbe8d2038d6392
v-kirilov <v_kirilov87@protonmail.ch>
2023-09-27 19:48:57 +0300

    Add block.timestamp variables.

--- PatrickCollinsCourse/Lottery_Lesson9/foundry-smart-contract-lottery-f23/src/Raffle.sol
+++ PatrickCollinsCourse/Lottery_Lesson9/foundry-smart-contract-lottery-f23/src/Raffle.sol
@@ -33,19 +33,23 @@ contract Raffle {
     error Raffle_NotEnoughEthSent();
 
     uint256 private immutable i_entranceFee;
-    address payable[]  private s_players;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
     /** Events */
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee){
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() external payable{
-
+    function enterRaffle() external payable {
         //require(msg.value >= i_entranceFee, "Not enough ETH sent!")
-        if(msg.value<i_entranceFee){
+        if (msg.value < i_entranceFee) {
             revert Raffle_NotEnoughEthSent();
         }
 
@@ -56,11 +60,16 @@ contract Raffle {
     //1. Get a random number
     //2. Use the random number to pick a player
     //3. Be automatically called
-    function pickWinner() public {}
+    function pickWinner() external {
+        // check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
 
     /**Getter function */
 
-    function getEntranceFee() external view returns(uint256){
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
