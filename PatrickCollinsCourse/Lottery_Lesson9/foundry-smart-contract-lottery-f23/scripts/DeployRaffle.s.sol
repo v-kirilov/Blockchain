86e7b7ff8cd5ac42c155dcbf29c09692c3595691
v-kirilov <v_kirilov87@protonmail.ch>
2023-09-30 23:03:07 +0300

    Add run funciotn in DeployScript.

--- PatrickCollinsCourse/Lottery_Lesson9/foundry-smart-contract-lottery-f23/scripts/DeployRaffle.s.sol
+++ PatrickCollinsCourse/Lottery_Lesson9/foundry-smart-contract-lottery-f23/scripts/DeployRaffle.s.sol
@@ -6,5 +6,28 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle, HelperConfig) {}
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig()
+        (
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            entranceFee,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return raffle;
+    }
 }
