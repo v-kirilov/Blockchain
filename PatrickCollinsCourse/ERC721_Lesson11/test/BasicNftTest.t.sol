1ea39b989cc8fa9d0406922d47e19ac8d623af99
v-kirilov <v_kirilov87@protonmail.ch>
2023-10-18 22:31:46 +0300

    Add a test for Mint and Balance functionality.

--- PatrickCollinsCourse/ERC721_Lesson11/test/BasicNftTest.t.sol
+++ PatrickCollinsCourse/ERC721_Lesson11/test/BasicNftTest.t.sol
@@ -9,6 +9,9 @@ import {BasicNft} from "../src/BasicNft.sol";
 contract BasifNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string constant SHIBA =
+        "https://ipfs.io/ipfs/QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png";
 
     constructor() {
         deployer = new DeployBasicNft();
@@ -19,6 +22,20 @@ contract BasifNftTest is Test {
         string memory expectedName = "Dogie";
         string memory actualName = basicNft.name();
 
-        assert(keccak256(abi.encodePacked(expectedName))==keccak256(abi.encodePacked(actualName)));
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(SHIBA);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(SHIBA)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
     }
 }
