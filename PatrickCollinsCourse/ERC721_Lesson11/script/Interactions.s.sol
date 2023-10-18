196b1a887bb96ba49e5b9afb6feefcdb6a8bf805
v-kirilov <v_kirilov87@protonmail.ch>
2023-10-18 22:41:00 +0300

    Add interactions.s.sol script.

---
+++ PatrickCollinsCourse/ERC721_Lesson11/script/Interactions.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+
+contract MinBasiNft is Script {
+    string constant SHIBA =
+        "https://ipfs.io/ipfs/QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png";
+
+    function run() public {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(SHIBA);
+        vm.stopBroadcast();
+    }
+}
