1d61d3e58ca0d75ca32aa0955223c42776cb1cc9
v-kirilov <v_kirilov87@protonmail.ch>
2023-11-23 23:12:12 +0200

    Add mint function and custom errors for it.

--- PatrickCollinsCourse/USD_Lesson12/src/DecentralizedStableCoin.sol
+++ PatrickCollinsCourse/USD_Lesson12/src/DecentralizedStableCoin.sol
@@ -19,6 +19,7 @@ import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.so
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__MustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
     constructor() ERC20("DesentraliedStalkeCoin", "DSC") Ownable(msg.sender) {}
 
@@ -32,5 +33,20 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
             revert DecentralizedStableCoin__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+
+        _mint(_to, _amount);
+        return true;
     }
 }
