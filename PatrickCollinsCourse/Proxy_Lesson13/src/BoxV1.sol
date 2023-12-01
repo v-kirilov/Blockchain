04c3934d8ab3dea9b7116f8383770ecd9393af56
v-kirilov <v_kirilov87@protonmail.ch>
2023-12-01 20:30:24 +0200

    BoxV1 from Lesson 13

---
+++ PatrickCollinsCourse/Proxy_Lesson13/src/BoxV1.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {UUPSUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+
+// storage is stored in the proxy, NOT in the implementation
+
+// proxy -> deploy implementation -> call some initializer function
+
+// Proxy (number = 0) -> implementation (number =1)
+contract BoxV1 is Initializable,UUPSUpgradeable,OwnableUpgradeable{
+    uint256 internal number;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    // Anyone can upgrade, we don't care for that now.
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
