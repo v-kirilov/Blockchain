54322be995c8e6a8666b63599618fae9ba24310e
v-kirilov <v_kirilov87@protonmail.ch>
2023-12-01 20:30:56 +0200

    Scripts for Deploy and Upgrade Box from Lesson 13

---
+++ PatrickCollinsCourse/Proxy_Lesson13/script/UpgradeBox.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {ERC1967Proxy} from "../lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract UpgradeBox is Script {
+    function run() external returns (address) {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
+
+        vm.startBroadcast();
+        BoxV2 newBox = new BoxV2();
+        vm.stopBroadcast();
+        address proxy = upgradeBox(mostRecentlyDeployed, address(newBox));
+        return proxy;
+    }
+
+    function upgradeBox(address proxyAddress, address newBox) public returns (address) {
+        vm.startBroadcast();
+        BoxV1 proxy = BoxV1(proxyAddress);
+        proxy.upgradeToAndCall(address(newBox),""); // Proxy contract now points to this new address
+        vm.stopBroadcast();
+        return address(proxy);
+    }
+}
