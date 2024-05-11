5fbe876ec94c33c43a630eb8a7f02dbfd2561e74
v-kirilov <v_kirilov87@protonmail.ch>
2024-05-11 16:40:02 +0300

    Rework BJDataFeed.

--- BlackJack/src/BlackJackDataFeed.sol
+++ BlackJack/src/BlackJackDataFeed.sol
@@ -3,31 +3,31 @@ pragma solidity 0.8.25;
 
 import {AggregatorV3Interface} from "../lib/foundry-chainlink-toolkit/src/interfaces/feeds/AggregatorV3Interface.sol";
 
-contract BlackJackDataFeed{
+contract BlackJackDataFeed {
     AggregatorV3Interface internal dataFeed;
 
-        /**
+    /**
      * Network: Sepolia
      * Aggregator: ETH/USD
      * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
      */
-
-       constructor() {
-        dataFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
+    constructor(address _dataFeedAddress) {
+        dataFeed = AggregatorV3Interface(_dataFeedAddress);
     }
 
-        /**
+    /**
      * Returns the latest answer.
      */
-    function getChainlinkDataFeedLatestAnswer() public view returns (int) {
+    function getChainlinkDataFeedLatestAnswer() public view returns (int256) {
         // prettier-ignore
         (
-            /* uint80 roundID */,
-            int answer,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
+            /* uint80 roundID */
+            ,
+            int256 answer,
+            /*uint startedAt*/
+            ,
+            /*uint timeStamp*/
+            ,
             /*uint80 answeredInRound*/
         ) = dataFeed.latestRoundData();
         return answer;
