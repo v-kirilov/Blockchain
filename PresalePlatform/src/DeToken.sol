ef8b7814a11313b95fdced8a6b7d0208cbc1f8d6
v-kirilov <v_kirilov87@protonmail.ch>
2024-06-02 22:39:29 +0300

    Simple implementation of a ERC20 token.

---
+++ PresalePlatform/src/DeToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity 0.8.25;
+
+contract DeToken is ERC20, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) Ownable(msg.sender){}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
