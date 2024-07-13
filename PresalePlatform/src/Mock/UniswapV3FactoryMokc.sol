59488fe7dc42b5b77a594a8057b8668d64c83654
v-kirilov <v_kirilov87@protonmail.ch>
2024-07-13 14:35:42 +0300

    Add logic to UniswapMock Address.

--- PresalePlatform/src/Mock/UniswapV3FactoryMokc.sol
+++ PresalePlatform/src/Mock/UniswapV3FactoryMokc.sol
@@ -2,7 +2,19 @@
 pragma solidity 0.8.25;
 
 contract UniswapV3FactoryMock {
-    function getPool(address , address, uint24) public pure returns (address) {
-        return 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    uint256 public pool = 0;
+
+    function getPool(address, address, uint24) public view returns (address) {
+        if (pool == 0) {
+            return address(0);
+        } else if (pool == 1) {
+            return address(1);
+        } else {
+            return 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+        }
+    }
+
+    function setPool(uint256 _pool) external {
+        pool = _pool;
     }
 }
