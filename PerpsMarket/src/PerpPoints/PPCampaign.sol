b569058e1cb1fea37ddc0774e6f040d6a4166f81
v-kirilov <v_kirilov87@protonmail.ch>
2025-03-20 22:43:23 +0200

    Add multiple variables , add comments and usage of SafeERC20 for handling transfers.

--- PerpsMarket/src/PerpPoints/PPCampaign.sol
+++ PerpsMarket/src/PerpPoints/PPCampaign.sol
@@ -2,21 +2,37 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract PPCampaign is AccessControl {
+    using SafeERC20 for IERC20;
+
     ///-///-///-///
     // Errors
     ///-///-///-///
     error Unauthorized();
     error ZeroAddress();
+    error MaxCamapignDurationExceeded();
 
+    ///-///-///-///
+    // Constants
+    ///-///-///-///
     bytes32 public constant CAMPAIGN_ADMIN_ROLE = keccak256("CAMPAIGN_ADMIN_ROLE");
 
+    ///-///-///-///
+    // Immutables
+    ///-///-///-///
+    uint256 public immutable MAX_CAMPAIGN_DURATION = 30 days;
     uint256 private immutable Duration;
     address private immutable PrizeToken;
+
+    ///-///-///-///
+    // Public variables
+    ///-///-///-///
     uint256 public firstPrizeAmount;
-    uint256 public immutable secontPrizeAmount;
-    uint256 public immutable thirdPrizeAmount;
+    uint256 public secontPrizeAmount;
+    uint256 public thirdPrizeAmount;
 
     //When participating in the campaign a user gains points based on his trading, if it's succesfull he gets more points.
     //At the end of a campaign the top 3 users with the most points will get a prize. The prize is a token that is set at the start of the campaign.
@@ -28,19 +44,21 @@ contract PPCampaign is AccessControl {
         uint256 prizePoints;
     }
 
-    
     //Points for each participant that has accumulated in the campaign
-    mapping(address => Participant) public participantsPoints;
-
+    mapping(address => Participant) public participants;
 
     constructor(uint256 _duration, address _prizeToken, address _campaignAdmin) {
         if (_duration == 0) {
             revert("Duration cannot be 0");
         }
+        if (_duration > MAX_CAMPAIGN_DURATION) {
+            revert MaxCamapignDurationExceeded();
+        }
 
         if (_prizeToken == address(0)) {
             revert ZeroAddress();
         }
+
         _grantRole(CAMPAIGN_ADMIN_ROLE, _campaignAdmin);
         Duration = _duration;
         PrizeToken = _prizeToken;
@@ -52,5 +70,21 @@ contract PPCampaign is AccessControl {
             revert Unauthorized();
         }
         _;
+    }
+
+    function setPrizeAmounts(uint256 _firstPrizeAmount, uint256 _secondPrizeAmount, uint256 _thirdPrizeAmount)
+        external
+        onlyCampaignAdmin
+    {
+        firstPrizeAmount = _firstPrizeAmount;
+        secontPrizeAmount = _secondPrizeAmount;
+        thirdPrizeAmount = _thirdPrizeAmount;
+    }
+
+    function claimPrize() external {
+        Participant storage participant = participants[msg.sender];
+        require(participant.campaignId == block.timestamp, "You are not a participant in the current campaign");
+        require(participant.prizePoints > 0, "You have no prize points to claim");
+        IERC20(PrizeToken).safeTransfer(msg.sender, participant.prizePoints);
     }
 }
